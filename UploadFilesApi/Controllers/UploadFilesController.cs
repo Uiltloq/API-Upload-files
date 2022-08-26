9ff5703402052fa22490367b547b644b9d1202d4
Uiltloq <uiltloq@gmail.com>
2022-08-26 16:56:20 +0300

    Create project and create UploadFilesController

---
+++ UploadFilesApi/Controllers/UploadFilesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UploadFilesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadFilesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(IFormFile formFile)
+        {
+            var uploadPath = $"{Directory.GetCurrentDirectory()}/uploads";
+
+            Directory.CreateDirectory(uploadPath);
+
+            string fullPath = Path.Combine(uploadPath, formFile.FileName);
+            using (var fileStream = new FileStream(fullPath, FileMode.CreateNew))
+            {
+                await formFile.CopyToAsync(fileStream);
+            }
+
+            return Ok();
+        }
+    }
+}
