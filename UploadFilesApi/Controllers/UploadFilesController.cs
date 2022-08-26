7717184443a3450156cbc0f3f1521cd955ca7c46
Uiltloq <uiltloq@gmail.com>
2022-08-26 17:49:23 +0300

    Fix large upload files

--- UploadFilesApi/Controllers/UploadFilesController.cs
+++ UploadFilesApi/Controllers/UploadFilesController.cs
@@ -6,17 +6,27 @@ namespace UploadFilesApi.Controllers
     [ApiController]
     public class UploadFilesController : ControllerBase
     {
+        [RequestFormLimits(ValueLengthLimit = int.MaxValue, MultipartBodyLengthLimit = int.MaxValue)]
+        [DisableRequestSizeLimit]
         [HttpPost]
         public async Task<ActionResult> PostAsync(IFormFile formFile)
         {
+
             var uploadPath = $"{Directory.GetCurrentDirectory()}/uploads";
 
             Directory.CreateDirectory(uploadPath);
-
+            
             string fullPath = Path.Combine(uploadPath, formFile.FileName);
-            using (var fileStream = new FileStream(fullPath, FileMode.CreateNew))
+            try
             {
-                await formFile.CopyToAsync(fileStream);
+                using (var fileStream = new FileStream(fullPath, FileMode.CreateNew))
+                {
+                    await formFile.CopyToAsync(fileStream);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new ArgumentException(ex.Message, ex);
             }
 
             return Ok();
